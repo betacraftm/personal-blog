10e3a58ea7aeedb30dd588b3d0b65145e198c8d8
Betacraft <hoangkimc9@gmail.com>
2024-07-05 22:33:30 +0700

    test api route

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import connectDB from './config/db.config.js'
+import userRoutes from './routes/user.routes.js'
 
 dotenv.config()
 connectDB()
 const app = express()
+
+app.use('/api/user', userRoutes)
 
 mongoose.connection.once('open', () => {
 	console.log('Connected to DB')
