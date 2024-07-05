f533e5e6c9a4d00655ae4a90223d0b0aa8b75aa3
Betacraft <hoangkimc9@gmail.com>
2024-07-05 22:04:51 +0700

    connect to db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import connectDB from './config/db.config.js'
 
+dotenv.config()
+connectDB()
 const app = express()
 
-app.listen(8904, () => {
-	console.log(`Server is running on PORT 8904`)
+mongoose.connection.once('open', () => {
+	console.log('Connected to DB')
+	app.listen(8904, () => {
+		console.log(`Server is running on PORT 8904`)
+	})
 })
