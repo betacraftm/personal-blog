2962ec4b347587aa14fb86e68549d1c278b11c13
Betacraft <hoangkimc9@gmail.com>
2024-07-06 23:35:08 +0700

    create signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
 	try {
@@ -39,6 +40,48 @@ export const signup = async (req, res) => {
 			.json({ message: `User ${username} created` })
 	} catch (error) {
 		console.log('Error in signup route')
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
+	}
+}
+
+export const signin = async (req, res) => {
+	try {
+		const { email, password } = req.body
+
+		if (!email || !password || email === '' || password === '') {
+			return res
+				.status(StatusCodes.BAD_REQUEST)
+				.json({ message: 'All field are required' })
+		}
+
+		const validUser = await User.findOne({ email })
+		if (!validUser) {
+			return res
+				.status(StatusCodes.NOT_FOUND)
+				.json({ message: 'User not found' })
+		}
+
+		const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+		if (!validPassword) {
+			return res
+				.status(StatusCodes.BAD_REQUEST)
+				.json({ message: 'Invalid password' })
+		}
+
+		const token = jwt.sign(
+			{ _id: validUser._id, username: validUser.username },
+			process.env.JWT_SECRET
+		)
+
+		const { password: pwd, ...rest } = validUser._doc
+
+		res
+			.status(StatusCodes.OK)
+			.cookie('access_token', token, { httpOnly: true })
+			.json(rest)
+	} catch (error) {
+		console.log('Error in signin route')
 		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message })
 	}
 }
