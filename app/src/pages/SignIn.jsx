80a95167056d70b928e4e7d9eaf67979fce02789
Betacraft <hoangkimc9@gmail.com>
2024-07-06 23:44:52 +0700

    complete signin functionality

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -1,3 +1,103 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../api/axios";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all the fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      await axios.post("/api/auth/signin", formData);
+      setLoading(false);
+      setFormData({});
+      navigate("/");
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+      setLoading(false);
+      console.log(errorMessage);
+    }
+  };
+
+  return (
+    <div className="mt-20 min-h-screen">
+      <div className="mx-auto flex max-w-3xl flex-col gap-5 p-3 md:flex-row md:items-center">
+        <div className="flex-1">
+          <Link to="/" className="text-4xl font-bold dark:text-white">
+            <span className="rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-2 py-1 text-white">
+              Betacraft's
+            </span>
+            Blog
+          </Link>
+          <p className="mt-5 text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+            corrupti odit doloremque quam officia illo ab. Doloribus facilis
+            nesciunt, sapiente voluptates repudiandae eum harum iure ea facere,
+            maxime debitis ex.
+          </p>
+        </div>
+
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="*********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="mt-5 flex gap-1 text-sm">
+            <span>Don't have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
