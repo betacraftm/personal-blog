c2275932c87330ed8d0f9a960ef1a6f924a69c9f
Betacraft <hoangkimc9@gmail.com>
2024-07-09 09:59:23 +0700

    complete sidebar

--- app/src/pages/DashBoard.jsx
+++ app/src/pages/DashBoard.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 export default function DashBoard() {
-  return <div>DashBoard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="flex min-h-screen flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
