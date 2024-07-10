c0f338f7dc18ab0078811d3a90bbca12eae8f5fb
Betacraft <hoangkimc9@gmail.com>
2024-07-10 23:01:03 +0700

    complete upload image functionality

--- app/src/components/DashProfile.jsx
+++ app/src/components/DashProfile.jsx
@@ -1,19 +1,82 @@
 import { useSelector } from "react-redux";
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+  const handleImmageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
+  const uploadImage = () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        (error) => {
+          setImageFileUploadError("Could not upload image");
+          setImageFile(null);
+          setImageUrl(null);
+        };
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((dowloadURL) => {
+          setImageUrl(dowloadURL);
+        });
+      },
+    );
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
   return (
     <div className="mx-auto w-full max-w-lg p-3">
       <h1 className="my-7 text-center text-3xl font-semibold">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="h-32 w-32 cursor-pointer self-center overflow-hidden rounded-full shadow-md">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImmageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="h-32 w-32 cursor-pointer self-center overflow-hidden rounded-full shadow-md"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
-            src={currentUser.profilePicture}
+            src={imageUrl || currentUser.profilePicture}
             alt="user"
             className="h-full w-full rounded-full border-8 border-[lightgray] object-cover"
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
+
         <TextInput
           type="text"
           id="username"
