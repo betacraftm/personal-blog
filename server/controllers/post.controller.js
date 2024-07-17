7c21afdec9ef178968d982bd6fff3fd39392316b
Betacraft <hoangkimc9@gmail.com>
2024-07-17 17:21:27 +0700

    create get posts api route

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -42,3 +42,51 @@ export const createPost = async (req, res) => {
 		res.status(StatusCodes).json({ message: error.message })
 	}
 }
+
+export const getPosts = async (req, res) => {
+	try {
+		const startIndex = parseInt(req.query.startIndex) || 0
+		const limit = parseInt(req.query.limit) || 9
+		const sortDirection = req.query.order === 'asc' ? 1 : -1
+		const posts = await Post.finds(
+			...(req.query.userId && { userId: req.query.userId }),
+			...(req.query.category && { category: req.query.category }),
+			...(req.query.slug && { slug: req.query.slug }),
+			...(req.query.postId && { _id: req.query.postId }),
+			...(req.query.searchTerm && {
+				$or: [
+					{ title: { $regex: req.query.searchTerm, $option: 'i' } },
+					{ content: { $regex: req.query.searchTerm, $option: 'i' } },
+				],
+			})
+		)
+			.sort({ updateAt: sortDirection })
+			.skip(startIndex)
+			.limit(limit)
+
+		const totalPosts = await Post.countDocuments()
+
+		const now = new Date()
+
+		const oneMonthAgo = new Date(
+			now.getFullYear(),
+			now.getMonth() - 1,
+			now.getDate()
+		)
+
+		const lastMonthPosts = await Post.countDocuments({
+			createdAt: { $gte: oneMonthAgo },
+		})
+
+		res.status(StatusCodes.OK).json({
+			posts,
+			totalPosts,
+			lastMonthPosts,
+		})
+	} catch (error) {
+		console.log('Error in getPosts', error.message)
+		res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.json({ message: error.message })
+	}
+}
