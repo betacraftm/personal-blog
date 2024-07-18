e6b9fafe1c7612e18047580f19dbb60711966a8b
Betacraft <hoangkimc9@gmail.com>
2024-07-18 23:01:33 +0700

    show user post inside dashboard

--- app/src/components/DashPost.jsx
+++ app/src/components/DashPost.jsx
@@ -1,3 +1,90 @@
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
+import { useSelector } from "react-redux";
+import { Table } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 export default function DashPost() {
-  return <div>DashPost</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get(
+          `/api/post/getposts?userId=${currentUser._id}`,
+        );
+        setUserPosts(res.data.posts);
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div className="scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 table-auto overflow-x-scroll p-3 md:mx-auto">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <Table hoverable className="shadow-md">
+          <Table.Head>
+            <Table.HeadCell>Date Updated</Table.HeadCell>
+            <Table.HeadCell>Post Image</Table.HeadCell>
+            <Table.HeadCell>Post Title</Table.HeadCell>
+            <Table.HeadCell>Catagory</Table.HeadCell>
+            <Table.HeadCell>Delete</Table.HeadCell>
+            <Table.HeadCell>Edit</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {userPosts.map((post) => {
+              return (
+                <Table.Row
+                  key={post._id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="h-10 w-20 bg-gray-500 object-cover"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      to={`/post/${post.slug}`}
+                      className="font-medium text-gray-900 dark:text-white"
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="cursor-pointer font-medium text-red-500 hover:underline">
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/update-post/${post._id}`}>
+                      <span className="text-teal-500 hover:underline">
+                        Edit
+                      </span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      ) : (
+        <p>You have no post yet!</p>
+      )}
+    </div>
+  );
 }
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -48,17 +48,17 @@ export const getPosts = async (req, res) => {
 		const startIndex = parseInt(req.query.startIndex) || 0
 		const limit = parseInt(req.query.limit) || 9
 		const sortDirection = req.query.order === 'asc' ? 1 : -1
-		const posts = await Post.finds(
-			...(req.query.userId && { userId: req.query.userId }),
-			...(req.query.category && { category: req.query.category }),
-			...(req.query.slug && { slug: req.query.slug }),
-			...(req.query.postId && { _id: req.query.postId }),
-			...(req.query.searchTerm && {
+		const posts = await Post.find(
+			req.query.userId && { userId: req.query.userId },
+			req.query.category && { category: req.query.category },
+			req.query.slug && { slug: req.query.slug },
+			req.query.postId && { _id: req.query.postId },
+			req.query.searchTerm && {
 				$or: [
 					{ title: { $regex: req.query.searchTerm, $option: 'i' } },
 					{ content: { $regex: req.query.searchTerm, $option: 'i' } },
 				],
-			})
+			}
 		)
 			.sort({ updateAt: sortDirection })
 			.skip(startIndex)
